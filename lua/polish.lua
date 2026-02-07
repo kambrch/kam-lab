87181ba4d2b99d990576377fc57d9f09be59249d
Kamil 'Kamaś' Bruchal <kamil.bruchal.pro@gmail.com>
2026-02-07 13:22:48 +0100

    spelling check

--- lua/polish.lua
+++ lua/polish.lua
@@ -4,3 +4,13 @@
 
 -- Sync all yanks to the system clipboard.
 vim.opt.clipboard = "unnamedplus"
+
+-- Enable spell checking for specific file types
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "markdown", "md", "tex", "latex", "plaintex", "rst", "asciidoc", "org", "mail" },
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = { "en", "pl" }  -- English and Polish
+  end,
+  desc = "Enable English and Polish spell checking for text files",
+})
