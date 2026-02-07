6ab2d0bfe32bdaf1b924bebd39fc3bba73d136e9
Kamil 'Kamaś' Bruchal <kamil.bruchal.pro@gmail.com>
2026-02-07 13:26:42 +0100

    spelling checks fix

--- changelog.md
+++
@@ -1,7 +0,0 @@
-=== 18 XI 2025 ===
-1) Inlay hints enabled (lua/plugins/inlay_hints.lua)
-2) Feline statusline, statuscolumn, and winbar
-3) Smartcolumn
-4) Vim-illuminate
-5) Highlight colors
-6) Tint neovim
--- lua/polish.lua
+++ lua/polish.lua
@@ -10,7 +10,18 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "text", "markdown", "md", "tex", "latex", "plaintex", "rst", "asciidoc", "org", "mail" },
   callback = function()
     vim.opt_local.spell = true
-    vim.opt_local.spelllang = { "en", "pl" }  -- English and Polish
+    -- Check if Polish spell file is available, otherwise use only English
+    local spell_dir = vim.fn.stdpath("data") .. "/site/spell"
+    local pl_spell_available = vim.fn.isdirectory(spell_dir) == 1 and 
+                              (vim.fn.glob(spell_dir .. "/pl.*.spl") ~= "")
+    
+    if pl_spell_available then
+      vim.opt_local.spelllang = { "en", "pl" }  -- English and Polish
+    else
+      vim.opt_local.spelllang = "en"  -- Only English if Polish is not available
+      -- Optionally notify user that Polish spell check is not available
+      vim.notify("Polish spell check not available. Install Polish spell files to enable.", vim.log.levels.WARN)
+    end
   end,
   desc = "Enable English and Polish spell checking for text files",
 })
